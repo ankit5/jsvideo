758ac6ed09d86540643dca8c97ec2276331d6ecb
ankit5 <vermankit5@gmail.com>
2024-01-26 12:10:47 +0530

    asf

--- player.php
+++ player.php
@@ -65,7 +65,7 @@
 
 
             <video id="video" class="video-js vjs-default-skin" controls preload="auto" playsinline="playsinline" poster="" tabindex="-1" role="application" muted="muted" autoplay="">
-                <source src="<?php urldecode($_GET['url'])?>" type="application/x-mpegURL">
+                <source src="https://soul.ydc1wes.me/hls2/02/00005/buegmfigy64l_l/master.m3u8?t=Dq0NR8meABOWvAH96f3w-talWSYeCVn00o7GEUG5BEY&s=1706251122&e=14400&f=29289&i=0.0&sp=0" type="application/x-mpegURL">
             </video>
         
         <script>
